864aea9fc558f6d4f3b9e913de042d85c6ce31af
Felipedge <fpalmasaldivia@gmail.com>
2022-08-04 20:27:49 -0400

    se agregan al archivo js las variables a utiliar

---
+++ assest/js/script.js
@@ -0,0 +1,7 @@
+const montoIngresado = document.querySelector ("#montoIngresado")
+const selector = document.querySelector("#selector")
+const btnResultado = document.querySelector("#btnResultado")
+const total = document.querySelector("#total")
+const grafico = document.querySelector("#grafico")
+
+
